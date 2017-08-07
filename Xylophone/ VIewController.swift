96381b57b5af446503166f2bc293133f7970ce76
Jonathan Go <jogo88@gmail.com>
2017-08-07 22:07:26 +0900

    finished app

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,6 +12,7 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer: AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,23 +22,32 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+        playSound(soundFileName: soundArray[sender.tag - 1])
+    }
+
+    func playSound(soundFileName: String) {
+        
         // sets the location or path, make sure to add it to your project!
-        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
         do {
-         
+            
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
             
         }
+        catch {
+            
+            print(error)
+        }
+        
+        audioPlayer.play()
     }
-
 }
 
 /*
 1. import AVFoundation
 2. add AVAudioPlayerDelegate in the class
 3. Declare a new variable for the player
-4. Create a soundURL that contains our sound file.
-5. 
- 
+4. Create a soundURL that contains our sound file inside notePressed func.
+5. Initialize or set up the audioplayer with the sound file (imagine putting a disc inside the cd player)
 */
